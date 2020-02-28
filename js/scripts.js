e656e7281e732af821745eb604c08ca8bcefcef2
Pip Bbaale <namugera.philip@gmail.com>
2020-02-28 12:25:58 +0300

    adding js and jquery to crust

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,63 @@
 var sizePrice, crustPrice;
+
 function sizeSelect(size){
     this.size=size;
 }
 
+function crustSelect(crust){
+    this.crust=crust;
+}
 
 $(document).ready(function(){
+
+
    $(".check_size").click(function(){
        var selectedSize = $("input[name='size']:checked").val();
        if(selectedSize=='small'){
         sizePrice = 250;
-           alert("Your are a - " + selectedSize+" "+sizePrice);
+           alert(selectedSize+" "+sizePrice);
         }
         else if(selectedSize=='medium'){
             sizePrice = 500;
-            alert("Your are a - " + selectedSize+sizePrice);
+            alert(selectedSize+sizePrice);
          }
          else if(selectedSize=='large'){
             sizePrice = 1000;
-            alert("Your are a - " + selectedSize+sizePrice);
+            alert(selectedSize+sizePrice);
          }
          size=selectedSize;
          console.log(size);
-   })
+   });
 
-    
+
+    $(".check_crust").click(function(){
+        var selectedCrust = $("input[name='crust']:checked").val();
+        if(selectedCrust=='thin'){
+            crustPrice = 12;
+            alert(selectedCrust+crustPrice);
+        }
+        else if(selectedCrust=='thick'){
+            crustPrice = 30;
+            alert(selectedCrust+crustPrice);
+        }
+        else if(selectedCrust=='stuffed'){
+            crustPrice = 35;
+            alert(selectedCrust+crustPrice);
+        }
+        else if(selectedCrust=='crispy'){
+            crustPrice = 20;
+            alert(selectedCrust+crustPrice);
+        }
+        else if(selectedCrust=='cheese'){
+            crustPrice = 25;
+            alert(selectedCrust+crustPrice);
+        }
+        else if(selectedCrust=='gluten'){
+            crustPrice = 35;
+            alert(selectedCrust+crustPrice);
+        }
+        crust=selectedCrust;
+        console.log(crust);
+
+    });
 });

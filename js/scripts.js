5d9553d03884bb1be98257d0c3dad6594edc9fef
Pip Bbaale <namugera.philip@gmail.com>
2020-02-28 11:17:51 +0300

    adding functions to my pizza size fields

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var sizePrice;
+function sizeSelect(size){
+    this.size=size;
+}
+
+
+$(document).ready(function(){
+   $(".check_size").click(function(){
+       var selectedSize = $("input[name='size']:checked").val();
+       if(selectedSize=='small'){
+        sizePrice = 250;
+           alert("Your are a - " + selectedSize+" "+sizePrice);
+        }
+        else if(selectedSize=='medium'){
+            sizePrice = 500;
+            alert("Your are a - " + selectedSize+sizePrice);
+         }
+         else if(selectedSize=='large'){
+            sizePrice = 1000;
+            alert("Your are a - " + selectedSize+sizePrice);
+         }
+         size=selectedSize;
+         console.log(size);
+   })
+
+    
+});
